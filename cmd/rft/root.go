01ccecd7689e5dd00ed9ce856a33ab50ce0ba53a
lucasew <lucas59356@gmail.com>
2026-06-22 14:23:32 -0300

    pprof flag
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmd/rft/root.go
+++ cmd/rft/root.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"github.com/lucasew/refactree/pkg/pprof"
 	"github.com/lucasew/refactree/pkg/version"
 	"github.com/spf13/cobra"
 )
 
 type rootOptions struct {
-	verbose bool
+	verbose  bool
+	pprofDir string
 }
 
 func Execute() error {
-	return newRootCmd().Execute()
+	cmd, profiler := newRootCmd()
+	// Always finalize profiles even when the command exits via error or signal
+	// handling that skips cobra PostRun in some edge cases.
+	defer profiler.Stop()
+	return cmd.Execute()
 }
 
-func newRootCmd() *cobra.Command {
+func newRootCmd() (*cobra.Command, *pprof.Profiler) {
 	opts := &rootOptions{}
+	profiler := &pprof.Profiler{}
 
 	cmd := &cobra.Command{
 		Use:           "rft",
@@ -22,9 +29,18 @@ func newRootCmd() *cobra.Command {
 		Version:       version.GetBuildID(),
 		SilenceUsage:  true,
 		SilenceErrors: true,
+		// Profiling runs only when --pprof-dir is set; hooks are no-ops otherwise.
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			profiler.Dir = opts.pprofDir
+			return profiler.Start()
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			profiler.Stop()
+		},
 	}
 
 	cmd.PersistentFlags().BoolVarP(&opts.verbose, "verbose", "v", false, "enable verbose logging")
+	cmd.PersistentFlags().StringVar(&opts.pprofDir, "pprof-dir", "", "optional; when set, write cpu/heap/memory/goroutine/allocs pprof profiles into this directory")
 
 	cmd.AddCommand(
 		newASTDumpCmd(opts),
@@ -36,5 +52,5 @@ func newRootCmd() *cobra.Command {
 		newServeCmd(opts),
 	)
 
-	return cmd
+	return cmd, profiler
 }
