3af14841c05fa0fd9d0f26db0e028c01a03acba2
lucasew <lucas59356@gmail.com>
2026-07-16 19:14:27 +0000

    fix(python): rename methods on Optional/Union/cast typed locals
    Optional[T], Union[T, None], T | None, and cast(T, x) left locals
    untyped for ExtraRename. With a foreign same-leaf method, a.run()
    was skipped.
    
    Unwrap Optional/Union/| to the single non-None arm in pythonTypeName
    and bind a = cast(A, x) / typing.cast(A, x) assignments.

---
+++ testdata/mv/python_rename_method_optional_cast/input/app.py
@@ -0,0 +1,65 @@
+from typing import Optional, Union, cast
+
+
+class A:
+    def run(self):
+        return 1
+
+
+class B:
+    def run(self):
+        return 2
+
+
+def use_optional(a: Optional[A]):
+    if a is not None:
+        a.run()
+
+
+def use_optional_b(b: Optional[B]):
+    if b is not None:
+        b.run()
+
+
+def use_union_pipe(a: A | None):
+    if a is not None:
+        a.run()
+
+
+def use_union_pipe_b(b: B | None):
+    if b is not None:
+        b.run()
+
+
+def use_union_typing(a: Union[A, None]):
+    if a is not None:
+        a.run()
+
+
+def use_union_typing_b(b: Union[B, None]):
+    if b is not None:
+        b.run()
+
+
+def use_cast(x):
+    a = cast(A, x)
+    a.run()
+
+
+def use_cast_b(x):
+    b = cast(B, x)
+    b.run()
+
+
+def use_typing_cast(x):
+    a = typing_cast_alias(x)
+    return a
+
+
+def typing_cast_alias(x):
+    # attribute form: typing.cast
+    import typing
+
+    a = typing.cast(A, x)
+    a.run()
+    return a
