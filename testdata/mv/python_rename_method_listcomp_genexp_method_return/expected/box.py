e2d738680d5bf424f21ce5c5b92afc256e4e3818
lucasew <lucas59356@gmail.com>
2026-07-19 14:50:20 +0000

    fix(python): dual-class listcomp/genexp body method-return under-renames
    Type comprehension bodies that are Class() or method-return leaves so
    assigned and inline list/set/genexp peels rename A.run without touching B.

---
+++ testdata/mv/python_rename_method_listcomp_genexp_method_return/expected/box.py
@@ -0,0 +1,80 @@
+class A:
+    def execute(self) -> int:
+        return 1
+
+
+class B:
+    def run(self) -> int:
+        return 2
+
+
+class BoxA:
+    a: A
+
+    def __init__(self, a: A):
+        self.a = a
+
+    def get(self) -> A:
+        return self.a
+
+
+class BoxB:
+    b: B
+
+    def __init__(self, b: B):
+        self.b = b
+
+    def get(self) -> B:
+        return self.b
+
+
+def use_listcomp_mr(ba: BoxA, bb: BoxB) -> int:
+    xs = [ba.get() for _ in range(1)]
+    ys = [bb.get() for _ in range(1)]
+    return xs[0].execute() + ys[0].run()
+
+
+def use_listcomp_class() -> int:
+    xs = [A() for _ in range(1)]
+    ys = [B() for _ in range(1)]
+    return xs[0].execute() + ys[0].run()
+
+
+def use_listcomp_inline_mr(ba: BoxA, bb: BoxB) -> int:
+    return [ba.get() for _ in range(1)][0].execute() + [bb.get() for _ in range(1)][0].run()
+
+
+def use_listcomp_inline_class() -> int:
+    return [A() for _ in range(1)][0].execute() + [B() for _ in range(1)][0].run()
+
+
+def use_genexp_next_mr(ba: BoxA, bb: BoxB) -> int:
+    xs = (ba.get() for _ in range(1))
+    ys = (bb.get() for _ in range(1))
+    return next(xs).execute() + next(ys).run()
+
+
+def use_genexp_next_class() -> int:
+    xs = (A() for _ in range(1))
+    ys = (B() for _ in range(1))
+    return next(xs).execute() + next(ys).run()
+
+
+def use_genexp_inline_mr(ba: BoxA, bb: BoxB) -> int:
+    return next(ba.get() for _ in range(1)).execute() + next(bb.get() for _ in range(1)).run()
+
+
+def use_genexp_inline_class() -> int:
+    return next(A() for _ in range(1)).execute() + next(B() for _ in range(1)).run()
+
+
+def use_setcomp_mr(ba: BoxA, bb: BoxB) -> int:
+    xs = {ba.get() for _ in range(1)}
+    ys = {bb.get() for _ in range(1)}
+    return next(iter(xs)).execute() + next(iter(ys)).run()
+
+
+def use_setcomp_class() -> int:
+    xs = {A() for _ in range(1)}
+    ys = {B() for _ in range(1)}
+    return next(iter(xs)).execute() + next(iter(ys)).run()
